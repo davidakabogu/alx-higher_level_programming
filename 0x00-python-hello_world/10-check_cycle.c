aeb86ac6a9b9eb494ed2790e9267ad88008d2229
David King <akabogudavid@gmail.com>
2023-05-08 18:59:16 -0700

    python intro

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,12 +12,13 @@ int check_cycle(listint_t *list)
 	if (!list)
 		return (0);
 
-	do {
+	while (slow && fast && fast->next)
+	{
 		slow = slow->next;
 		fast = fast->next->next;
 		if (slow == fast)
 			return (1);
-	} while (slow && fast && fast->next);
+	}
 
 	return (0);
 }
