67776aaafcc56a90f57767702bccc011582a3b98
David King <akabogudavid@gmail.com>
2023-05-17 21:42:44 -0700

    more data structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -18,7 +18,7 @@ def roman_to_int(roman_string):
         return 0
 
     rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(rom_n.keys())
+    list_keys = list(rom_num.keys())
 
     num = 0
     last_rom = 0
