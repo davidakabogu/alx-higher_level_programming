3e9ea0b6c086ecc0df2582054e0ec13f4114e6b9
David King <akabogudavid@gmail.com>
2023-05-30 19:45:02 -0700

    object oriented programming

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
-
 
 class Square:
-    """Represent a square."""
+    """ Represents a square. """
 
     def __init__(self, size=0):
-        """Initialize a new square.
+        """
+        Initializes a new square.
 
         Args:
             size (int): The size of the new square.
@@ -16,11 +15,21 @@ class Square:
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        """ Gets or sets the current size of the square. """
+        return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Validates and sets the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -28,5 +37,5 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """ Calculates and returns the area of the square. """
+        return self.__size ** 2
