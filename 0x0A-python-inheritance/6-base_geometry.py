19b3250d2a80d06edb2fd5bf73b4b96a77fc9f35
David King <akabogudavid@gmail.com>
2023-06-11 16:10:33 -0700

    python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" creates a class called BaseGeometry """
+
+
+class BaseGeometry:
+    """ defines a function called area with self attribute """
+    def area(self):
+        """ not implemented """
+        raise Exception("area() is not implemented")
