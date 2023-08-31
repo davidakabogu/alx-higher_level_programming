95e15f62f34c4aa286b35f1373b452f98d2709f1
DavidAkabogu <Akabogudavid@gmail.com>
2023-08-31 22:37:30 +0100

    curl option

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script takes in a URL, sends a request, and displays the size of the response body
-curl -s -I "$1" | wc -c
+# Bash script takes in a URL, sends a request and displays size of response body
+curl -s "$1" | wc -c
