a9da95254c0dc3d5f5c1191833e861d81fed9b13
DavidAkabogu <Akabogudavid@gmail.com>
2023-09-01 00:22:03 +0100

    inteview task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,20 +2,17 @@
 # this function finds a peak in a list of unsorted integers.
 # a peak is a number that is greater than its adjacent numbers
 
-
 def find_peak(list_of_integers):
     # check if list is empty
     if not list_of_integers:
         return None
-
     # check is list is single digit
     if len(list_of_integers) == 1:
         return list_of_integers[0]
-
     # iterate through list
     for i in range(1, len(list_of_integers) - 1):
-        if (list_of_integers[i] >= list_of_integers[i + 1] and
-                list_of_integers[i] >= list_of_integers[i - 1]):
+        if (list_of_integers[i] > list_of_integers[i + 1] and
+                list_of_integers[i] > list_of_integers[i - 1]):
             return list_of_integers[i]
 
     # check first and last elements as the have only one neighbor
@@ -25,7 +22,6 @@ def find_peak(list_of_integers):
         return list_of_integers[-1]
 
     return None  # no peak found
-
 
 print(find_peak([1, 2, 4, 6, 3]))
 print(find_peak([4, 2, 1, 2, 3, 1]))
