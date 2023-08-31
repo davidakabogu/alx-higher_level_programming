4fd635b41e8add8a8937b5dd72fec72de265658e
DavidAkabogu <Akabogudavid@gmail.com>
2023-09-01 00:28:06 +0100

    inteview task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,12 +3,15 @@
 # a peak is a number that is greater than its adjacent numbers
 
 def find_peak(list_of_integers):
+
     # check if list is empty
     if not list_of_integers:
         return None
+    
     # check is list is single digit
     if len(list_of_integers) == 1:
         return list_of_integers[0]
+
     # iterate through list
     for i in range(1, len(list_of_integers) - 1):
         if (list_of_integers[i] > list_of_integers[i + 1] and
