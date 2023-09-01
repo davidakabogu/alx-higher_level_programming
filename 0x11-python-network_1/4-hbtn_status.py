9c3b724149744dae05cd03496e2f6d2eda80bef5
DavidAkabogu <Akabogudavid@gmail.com>
2023-09-01 18:34:11 +0100

    get requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,13 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        content = response.json()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-    else:
-        print("Error: Unable to fetch the URL. Status code:", response.status_code)
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
