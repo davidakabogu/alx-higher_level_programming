145b8599d2820af73dc37edbe4a1ad11bdc3400f
DavidAkabogu <Akabogudavid@gmail.com>
2023-09-02 12:31:01 +0100

    updates

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,8 +4,9 @@ import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
+    # response = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("http://0.0.0.0:5050/status")
     if response.status_code == 200:
         print("Body response:")
-        print("\t- type: {}".format(type(response.text)))
-        print("\t- content: {}".format(response.text))
+        print("\t- type: {}".format(type(r.text)))
+        print("\t- content: {}".format(r.text))
