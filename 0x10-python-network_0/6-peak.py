f49aeb48776ca97b08e0ae8ac0d86f918a1f5adc
DavidAkabogu <Akabogudavid@gmail.com>
2023-09-02 12:46:45 +0100

    remove examples

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -22,11 +22,3 @@ def find_peak(list_of_integers):
     if list_of_integers[-1] >= list_of_integers[-2]:
         return list_of_integers[-1]
     return None  # no peak found
-
-
-# print(find_peak([1, 2, 4, 6, 3]))
-# print(find_peak([4, 2, 1, 2, 3, 1]))
-# print(find_peak([2, 2, 2]))
-# print(find_peak([]))
-# print(find_peak([-2, -4, 2, 1]))
-# print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
